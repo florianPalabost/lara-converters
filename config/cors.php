b3db6accebcd2d27af53370d35697470dd22537d
Florian <palabostf@gmail.com>
2024-03-08 20:43:54 +0100

    chore(scramble): update cors setup for allow scramble

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
+    'paths'                    => ['api/*', 'sanctum/csrf-cookie', 'docs/api/*'],
 
     'allowed_methods'          => ['*'],
 
