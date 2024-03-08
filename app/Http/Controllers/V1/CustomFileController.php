1fa67cf9ccd5fdc440020f1bd25d726e61c4c18f
Florian <palabostf@gmail.com>
2024-03-08 21:22:24 +0100

    bug(files): fix wrong namespaces

--- app/Http/Controllers/V1/CustomFileController.php
+++ app/Http/Controllers/V1/CustomFileController.php
@@ -6,23 +6,18 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\StoreCustomFileRequest;
-use App\Http\Resources\CustomFileResource;
+use App\Http\Resources\V1\CustomFileResource;
 use App\Models\CustomFile;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CustomFileController extends Controller
 {
-    public function __construct()
-    {
-        // $this->middleware('auth:sanctum');
-    }
-
     /**
      * Get all custom files.
      *
-     * @return AnonymousResourceCollection<<CustomFileResource>
+     * @return AnonymousResourceCollection<CustomFileResource>
      */
     public function index(): AnonymousResourceCollection
     {
