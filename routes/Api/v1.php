bd82ed425a2e40cee9ed8ed112d305ac42853628
Florian <palabostf@gmail.com>
2024-03-08 20:44:27 +0100

    refactor(api): setup versioning api

---
+++ routes/Api/v1.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\V1\ConverterController;
+use App\Http\Controllers\V1\CustomFileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('convert', [ConverterController::class, 'convert']);
+Route::apiResource('files', CustomFileController::class);
