1486c4738065ccd62a965cf0068f665cab8f5d5a
Florian <palabostg@gmail.com>
2024-03-15 14:36:02 +0100

    refactor(auth): add conf forr socialite

--- config/app.php
+++ config/app.php
@@ -187,6 +187,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \SocialiteProviders\Manager\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -212,6 +213,7 @@ return [
 
     'aliases'         => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ])->toArray(),
 
 ];
